596e032a5c21b6cc286c5e40d70c787d7d78e962
trevor <thisistrevor4@gmail.com>
2017-06-23 17:25:56 -0500

    changed plus and minus btn color

--- app/src/main/java/com/stickyblob/metronome/MainActivity.java
+++ app/src/main/java/com/stickyblob/metronome/MainActivity.java
@@ -1,28 +1,37 @@
 package com.stickyblob.metronome;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private boolean toggle = false;
+    private boolean toggle = true;
     private long timeNow;
     private long timeOld = 0;
     private double totalSeconds;
     private double averageSeconds;
     private int divider = 1;
     boolean shouldSet = false;
+    private double bpm;
 
-    private TextView mTimeTv;
-    private TextView mAverageTimeTv;
+
+    private EditText mBPMinuteEditText;
+    private EditText mBPMeasureEditText;
+    private Button mTapToRhythmBtn;
+
+    Vibrator mVibrator;
 
 
     @Override
@@ -30,38 +39,97 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTimeTv = (TextView) findViewById(R.id.timeTv);
-        mAverageTimeTv = (TextView) findViewById(R.id.averageTimeTv);
+        mVibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+        mBPMinuteEditText = (EditText) findViewById(R.id.bp_minute_et);
+        mBPMeasureEditText = (EditText) findViewById(R.id.bp_measure_et);
+        mTapToRhythmBtn = (Button) findViewById(R.id.tap_to_rhythm_btn);
 
-    }
-
-    public void tapToRhythm(View view) {
-
-        long bpm = testTime();
-        mTimeTv.setText(Long.toString(bpm));
-    }
-
-    private void toggleFlashLight() {
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
-            toggle = !toggle;
-            try {
-                CameraManager cameraManager = (CameraManager) getApplicationContext().getSystemService(Context.CAMERA_SERVICE);
-
-                for (String id : cameraManager.getCameraIdList()) {
-                    if (cameraManager.getCameraCharacteristics(id).get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
-                        cameraManager.setTorchMode(id, toggle);
-                    }
+        mTapToRhythmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long bpm = testTime();
+                if (bpm > 400) {
+                    bpm = 0;
                 }
-            } catch (Exception e) {
-                Log.e(TAG, "toggleFlashLight: Cannot turn on flashlight", e);
+                mBPMinuteEditText.setText(Long.toString(bpm));
             }
+        });
+
+        mTapToRhythmBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                resetBeatsPerMinute();
+                mVibrator.vibrate(25);
+                return false;
+            }
+        });
+    }
+
+    // Button Callbacks
+    public void plusBPMinuteBtnClicked(View view) {
+        long number = Long.parseLong(mBPMinuteEditText.getText().toString());
+        long number2 = number + 1;
+        mBPMinuteEditText.setText(Long.toString(number2));
+    }
+
+    public void minusBPMinuteBtnClicked(View view) {
+        long number = Long.parseLong(mBPMinuteEditText.getText().toString());
+        if (number == 0) {
+            return;
+        }
+        long number2 = number - 1;
+        mBPMinuteEditText.setText(Long.toString(number2));
+    }
+
+    public void minusBPMeasureBtnClicked(View view) {
+
+    }
+
+    public void plusBPMeasureBtnClicked(View view) {
+
+    }
+
+    public void startBtnClicked(View view) {
+        resetBeatsPerMinute();
+        final int delay = 100;
+        final Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            public void run() {
+                toggle = true;
+                toggleFlashLight(2);
+                Log.d(TAG, "run: here");
+                h.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    public void stopBtnClicked(View view) {
+
+    }
+    // end of button callbacks
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void toggleFlashLight(int timeOn) {
+        toggle = !toggle;
+        try {
+            CameraManager cameraManager = (CameraManager) getApplicationContext().getSystemService(Context.CAMERA_SERVICE);
+
+            for (String id : cameraManager.getCameraIdList()) {
+                if (cameraManager.getCameraCharacteristics(id).get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
+                    cameraManager.setTorchMode(id, toggle);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "toggleFlashLight: Cannot turn on flashlight", e);
         }
     }
 
     private long testTime() {
         // get current time in millis
         timeNow = System.currentTimeMillis();
-        if(shouldSet) {
+        if (shouldSet) {
             long rawTimeDif = timeNow - timeOld;
             double seconds = rawTimeDif / 1000.00;
             totalSeconds = totalSeconds + seconds;
@@ -70,8 +138,16 @@ public class MainActivity extends AppCompatActivity {
         }
         shouldSet = true;
         timeOld = timeNow;
-        double bpm = (double) 60 / averageSeconds;
+        bpm = (double) 60 / averageSeconds;
         return Math.round(bpm);
+    }
+
+    private void resetBeatsPerMinute() {
+        divider = 0;
+        totalSeconds = 0;
+        timeOld = 0;
+
+        shouldSet = false;
     }
 }
 
